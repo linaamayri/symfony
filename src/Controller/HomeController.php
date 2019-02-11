7ab0b9953a50951916212a95e6c5f77af017717b
linaamayro <linaamayri@gmail.com>
2019-02-11 11:15:30 +0100

    premiercommit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linaa
+ * Date: 11/02/2019
+ * Time: 10:58
+ */
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class HomeController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first Symfony page');
+    }
+}
+
